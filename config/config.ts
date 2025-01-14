3f202774d7a37e8f3373a4e431f39a72eff07ea6
İbrahim Gurses <igrses9@gmail.com>
2025-01-14 14:38:29 +0300

    Docker config change

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,6 @@
-export const PARTNER_API_BASE_URL = "https://partner.peaka.studio/api/v1";
+export const PARTNER_API_BASE_URL = process.env.NEXT_PUBLIC_PARTNER_BASE_URL
+  ? process.env.NEXT_PUBLIC_PARTNER_BASE_URL
+  : "https://partner.peaka.studio/api/v1";
 export const API_KEY_LOCAL_STORAGE_KEY = "apiKeys";
 export const SELECTED_API_KEY_LOCAL_STORAGE_KEY = "selectedApiKey";
 export const ID_SEPARATOR_CHAR = "#";
