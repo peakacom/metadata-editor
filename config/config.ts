ad9516b0f49cf160914a13f451d73753caee6e57
İbrahim Gurses <igrses9@gmail.com>
2025-01-14 13:57:54 +0300

    Add docker file

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
-export const PARTNER_API_BASE_URL = "https://partner-test.peaka.host/api/v1";
+export const PARTNER_API_BASE_URL = "https://partner.peaka.studio/api/v1";
 export const API_KEY_LOCAL_STORAGE_KEY = "apiKeys";
 export const SELECTED_API_KEY_LOCAL_STORAGE_KEY = "selectedApiKey";
 export const ID_SEPARATOR_CHAR = "#";
