06c4221142a8558f20ad9306a202c32b8525ba5a
İbrahim Gurses <igrses9@gmail.com>
2025-02-12 14:42:17 +0300

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,4 @@
+console.log("Hasan" + process.env.NEXT_PUBLIC_PARTNER_BASE_URL);
 export const PARTNER_API_BASE_URL = process.env.NEXT_PUBLIC_PARTNER_BASE_URL
   ? process.env.NEXT_PUBLIC_PARTNER_BASE_URL
   : "https://partner.peaka.studio/api/v1";
